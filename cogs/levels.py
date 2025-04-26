4f2323417871ff77c6924f504211a72c08d63aeb
Philipp123623 <p.s.stueve@gmail.com>
2025-04-26 12:28:13 +0200

    Rank command rework 2

--- cogs/levels.py
+++ cogs/levels.py
@@ -384,7 +384,7 @@ class levelsystem(commands.Cog):
                             break
 
                 # --- Rank (#Platz) zeichnen
-                draw.text((397, 174), f"#{rank}", font=font_rank, fill="white")
+                draw.text((397, 160), f"#{rank}", font=font_rank, fill="white")
 
                 # LEVEL – vertikal & horizontal zentriert
                 level_text = f"{lvl_start}"
