5e590f6e138319ee4b34cfb2129c6e7089d66f3e
Philipp123623 <p.s.stueve@gmail.com>
2025-08-11 04:15:34 +0200

    Second Commit

--- cogs/help.py
+++ cogs/help.py
@@ -190,10 +190,10 @@ class HelpCog(commands.Cog):
         embed.add_field(
             name="Letzte Updates",
             value=(
-                "> <:Astra_file1:1141303837181886494> Neues Backup-System: `/backup create`, `/backup load`, `/backup undo`\n"
-                "> <:Astra_news:1141303885533827072> Neues Benachrichtigungssystem: `/benachrichtigung youtube`, `/benachrichtigung twitch`\n"
-                "> <:Astra_cookie:1141303831293079633> Neues Economy: `/job`, `/economy`, `/economy blackjack`\n"
-                "> <:Astra_minigames:1141303876528648232> Neue Minigames: `/hangman`, `/snake`, `/emojiquiz`"
+                "> <:Astra_file1:1141303837181886494> Backup-System: `/backup create`, `/backup load`, `/backup undo`\n"
+                "> <:Astra_news:1141303885533827072> Benachrichtigungssystem: `/benachrichtigung youtube/twitch`\n"
+                "> <:Astra_cookie:1141303831293079633> Economy: `/job`, `/economy`, `/economy blackjack`\n"
+                "> <:Astra_minigames:1141303876528648232> Minigames: `/hangman`, `/snake`, `/emojiquiz`"
             ),
             inline=False,
         )
